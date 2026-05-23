fd32218b733414a6d3d30fb139090009c66c9bc0
Andriel <andriel.h@aluno.ifsp.edu.br>
2026-05-22 22:06:53 -0300

    First Commit Test

---
+++ pdmde/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'login/login_page.dart';
+import 'home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const Color primaryColor = Color(0xFF1E3A8A);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recebimento Genérico',
+
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: const Color(0xFFF4F7FB),
+        useMaterial3: true,
+      ),
+
+      initialRoute: '/',
+
+      routes: {'/': (_) => const LoginPage(), '/home': (_) => const HomePage()},
+    );
+  }
+}
